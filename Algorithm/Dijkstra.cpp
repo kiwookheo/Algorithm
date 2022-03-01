f7bfd9a4835557c6765aa52203d959017cc67758
kiwookheo <goya121@naver.com>
2022-03-01 19:28:46 +0900

    Dijkstra with path

--- Algorithm/Dijkstra.cpp
+++ Algorithm/Dijkstra.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int d[20001];
+int pre[20001];
 const int INF = 1e9 + 10;
 vector<pair<int, int>> adj[20001];
 
 int main() {
-	int v, e, st;
-	cin >> v >> e >> st;
+	int v, e, st, en;
+	cin >> v >> e;
 	fill(d, d + v + 1, INF);
 
 	while (e--) {
@@ -15,7 +16,7 @@ int main() {
 		cin >> a >> b >> c;
 		adj[a].push_back({ c, b });
 	}
-
+	cin >> st >> en;
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // 우선순위 큐를 거리가 적은 순으로 정렬
 
 	d[st] = 0;
@@ -27,8 +28,20 @@ int main() {
 			if (d[nxt.second] <= d[cur.second] + nxt.first) continue; // 기존 최단 경로 테이블의 값이 더 작을 때 continue;
 			d[nxt.second] = d[cur.second] + nxt.first; // 다음 목적지에 대한 최단 경로 값 갱신
 			pq.push({ d[nxt.second], nxt.second }); // 최단 경로로 설정되면 pq에 push
+			pre[nxt.second] = cur.second; // 최단 경로 값이 갱신 될 때 pre 테이블도 갱신
 		}
 	}
+	cout << d[en] << '\n';
+	vector<int> path;
+	int cur = en;
+	while (cur != st) {
+		path.push_back(cur);
+		cur = pre[cur];
+	}
+	path.push_back(cur);
+	reverse(path.begin(), path.end());
+	cout << path.size() << '\n';
+	for (auto x : path) cout << x << ' ';
 	for (int i = 1; i <= v; i++) {
 		if (d[i] == INF) cout << "INF" << '\n';
 		else cout << d[i] << '\n';
